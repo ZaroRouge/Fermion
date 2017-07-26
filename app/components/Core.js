308ba3528cd77b3594ca6435e1ef124c22e9bc6b
Garrett Levy <garrettlr@gmail.com>
2017-07-26 14:54:03 -0700

    gooey beta final layout, needs functionality

--- app/components/Core.js
+++ app/components/Core.js
@@ -1,9 +1,10 @@
 //app/components
 import React, { Component } from 'react';
 import styles from './photon.css';
+import corestyles from './Core.css';
 import Left from './left';
 import Right from './right';
-
+import { Link } from 'react-router-dom';
 
 class Core extends Component {
   // props : {
@@ -12,16 +13,27 @@ class Core extends Component {
 
   render() {
     return (
-      <div className = {styles["window-content"]}>
-
-          <div className = {styles["pane-group"]}>
-            <Left />
-            <div className = {styles.pane} data-tid="AppContainer">
+      <div className = {styles["window-content"] + ' ' + corestyles.container}>
+        <div className = {styles["pane-group"]}>
+          <Left />
+          <div className = {styles.pane + ' ' + corestyles.main}>
+            <header className = {corestyles.footer + ' ' + corestyles.header}>
+              <h1 className={styles.title + ' ' + corestyles.title}>Web View</h1>
+            </header>
+            <div className = {corestyles.pads} data-tid="AppContainer">
               {/*this is the main component*/}
             </div>
-            <Right />
+            <footer className ={corestyles.footer}>
+              <h1 className = {styles.title + ' ' + corestyles.title}>Footer</h1>
+              <div className={corestyles.backButton} data-tid="backButton">
+                <Link to="/">
+                  <i className="fa fa-arrow-left" />
+                </Link>
+              </div>
+            </footer>
           </div>
-
+          <Right />
+        </div>
       </div>
     )
   }
--- app/components/Left.js
+++ app/components/Left.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 import styles from './photon.css';
 import buttonstyle from './Core.css';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import Tree from './CompTree.js';
+import List from './CompList.js';
 function Left(props){
   return (
     <div className = {styles["pane-med"] + " " + buttonstyle.sidebar}>
-      <div className={buttonstyle.backButton} data-tid="backButton">
-        <Link to="/">
-          <i className="fa fa-arrow-left fa-3x" />
-        </Link>
-      </div>
-      {/* probably want some stuff here */}
 
+      {/* probably want some stuff here */}
+    <Tree />
+    <List />
     </div>
   )
 }
